11fc4f1e919abdde524bfd2a300bb12073479cdc
dparmar951 <44137923+dparmar951@users.noreply.github.com>
2023-09-07 14:40:52 +0530

    Mock Data

---
+++ Backend/Data.js
@@ -0,0 +1,23 @@
+const mockData = [
+  {
+    date: '10-10-2022T13:12:22',
+    gross_amount: 1000,
+    status: 'initiated',
+    customer: 'Eren Akichi',
+    swifter_id: '2342WM',
+    external_id: 'T234S2',
+    source: 'ecommerce',
+  },
+  {
+    date: '10-10-2022T23:12:22',
+    gross_amount: 2000,
+    status: 'authorized',
+    customer: 'Eren Akichi',
+    swifter_id: '2342WM',
+    external_id: 'T234S2',
+    source: 'ecommerce',
+  },
+  // Add more mock data here
+];
+
+module.exports = mockData;
